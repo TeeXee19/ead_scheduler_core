1f3a5050e51b14735f1a4f3211c335697f8af826
Tochukwu  Franklin Ene <teec3000@gmail.com>
2020-09-20 13:57:55 +0100

    Ignored the default security config,and successfully ran spring boot.

--- src/main/java/com/interra/ead_scheduler_core/EadSchedulerCoreApplication.java
+++ src/main/java/com/interra/ead_scheduler_core/EadSchedulerCoreApplication.java
@@ -2,12 +2,18 @@ package com.interra.ead_scheduler_core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.web.bind.annotation.*;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@RestController
 public class EadSchedulerCoreApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EadSchedulerCoreApplication.class, args);
 	}
-
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
+    }
 }
