7146411bed003cc0d9e52ab6965be5cc389d099c
Tochukwu  Franklin Ene <teec3000@gmail.com>
2020-09-20 10:09:37 +0100

    Initial commit

---
+++ src/main/java/com/interra/ead_scheduler_core/EadSchedulerCoreApplication.java
@@ -0,0 +1,13 @@
+package com.interra.ead_scheduler_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EadSchedulerCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EadSchedulerCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/interra/ead_scheduler_core/EadSchedulerCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interra.ead_scheduler_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EadSchedulerCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
