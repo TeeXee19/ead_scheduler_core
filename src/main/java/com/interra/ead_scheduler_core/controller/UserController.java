849c8de51c2b15b8840331c79eb2877a08644bac
TeeXee19 <teec3000@gmail.com>
2020-09-30 09:45:11 +0100

    deleted replicated dependencies from th pom.xml Added the mapping for registration on the controller

--- src/main/java/com/interra/ead_scheduler_core/controller/UserController.java
+++ src/main/java/com/interra/ead_scheduler_core/controller/UserController.java
@@ -5,25 +5,54 @@ import java.util.List;
 import com.interra.ead_scheduler_core.model.User;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-import com.interra.ead_scheduler_core.response.ESResponse;
-import com.interra.ead_scheduler_core.response.ResponseParams;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import com.interra.ead_scheduler_core.response.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.Valid;
 
 @RestController
 public class UserController extends Controller {
- 
+
     @RequestMapping(value = "/login/", method = RequestMethod.GET)
     public ResponseEntity<ESResponse> listAllUsers() {
         ESResponse result;
         List<User> users = userService.findAllUsers();
-        if(users.isEmpty()){
+        if (users.isEmpty()) {
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);//You many decide to return HttpStatus.NOT_FOUND
         }
-        result=ResponseParams.buildSuccess(users);
+        result = ResponseParams.buildSuccess(users);
         return ResponseEntity.ok().body(result);
     }
-    
-    
+
+    @GetMapping(value = "/registration")
+    public ModelAndView registration() {
+        ModelAndView modelAndView = new ModelAndView();
+        User user = new User();
+        modelAndView.addObject("user", user);
+        modelAndView.setViewName("registration");
+        return modelAndView;
+    }
+
+    @PostMapping(value = "/registration")
+    public ModelAndView createNewUser(@Valid User user, BindingResult bindingResult) {
+        ModelAndView modelAndView = new ModelAndView();
+        User userExists = userService.findUserByUserName(user.getUserName());
+        if (userExists != null) {
+            bindingResult
+                    .rejectValue("userName", "error.user",
+                            "There is already a user registered with the user name provided");
+        }
+        if (!bindingResult.hasErrors()) {
+            userService.saveUser(user);
+            modelAndView.addObject("successMessage", "User has been registered successfully");
+            modelAndView.addObject("user", new User());
+
+        }
+        modelAndView.setViewName("registration");
+        return modelAndView;
+    }
+
+
 }
